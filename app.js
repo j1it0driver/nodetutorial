40f2421205239a2791514d84751205b1b2818485
TR JDO Ubuntu <j1it0driver@gmail.com>
2017-08-01 11:01:51 -0500

    My first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var lessMiddleware = require('less-middleware');
+
+var index = require('./routes/index'); //JDO Cundo requiero este archivo (index.js), estoy realmente llamando al funcion routes que esta dentro de index.js
+//var users = require('./routes/users');
+
+var app = express(); // JDO la aplicacion arranca aca cuando ejecutamos express.
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views')); // _dirname es el nombre de la aplicacion, su folder?. Path.join con / como un path en una URI
+app.set('view engine', 'hjs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(lessMiddleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+//app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+router.get('/', function(req,res){
+  res.send({users: [ "Wil", "Laura"
+
+  ]});
+});
+
+
+module.exports = router;  //JDO este archivo retorna el archivo router creado arriba
