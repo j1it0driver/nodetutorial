6f7a57612216ed9228d8501e2785dc9f6d6ae5fa
TR JDO Ubuntu <j1it0driver@gmail.com>
2017-08-31 11:42:32 +0200

    buttons disable ok2

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,36 +11,31 @@ version="20170810",
 $speechInput,
 $recBtn,
 recognition,
-mensaje,
 messageRecording = "Recording...",
 messageCouldntHear = "I couldn't hear you, could you say that again?",
 messageInternalError = "Oh no, there has been an internal server error",
 messageSorry = "I'm sorry, I don't have the answer to that yet.";
 var tiempoSend, timeout = null, arrayList=[], buttonId=[];;
-    var str="";
+var str="";
 // var firstTypedLetter = 'Y';
 $(document).ready(function() {   //////////////////////////////////// JS PRINCIPAL ////////////////////////////////////
     $speechInput = $("#speech");
     $recBtn = $("#rec");
     $recBtn1 = $("#rec1");
-    mensaje = $speechInput.val();
-    $speechInput.keyup(function(event) { //I change keyup fot keypress//
+    $speechInput.keyup(function(event) { //I change keyup for keypress//
         if (tiempoSend !== null) {
             clearTimeout(tiempoSend);
         }
         if (timeout !== null) {
             clearTimeout(timeout);
         }
-        $speechInput.val();
+        // $speechInput.val();
         if ($speechInput.val()==''){
-            // $('#statusMessages').text("Waiting input...");
-            // $('#statusMessages').text("\""+process.env.API_AI_CIENT_TOKEN_TADV+"\"");
-            // $('#statusMessages').text("clietTOKEN");
         }
         else{
             $('#statusMessages').text("Typing...");
-            $speechInput.val();
-                timeout = setTimeout(function () {if($speechInput.val() != ''){$('#statusMessages').text("Waiting input or Enter...");}}, 3000);
+            // $speechInput.val();
+            timeout = setTimeout(function () {if($speechInput.val() != ''){$('#statusMessages').text("Waiting input or Enter...");}}, 3000);
         }
         if (event.which == 13) {
             event.preventDefault();
@@ -60,15 +55,15 @@ $(document).ready(function() {   //////////////////////////////////// JS PRINCIP
 
 });
 
-function printInput() {          //////////////////////////////////// PRINT FUNCTIONS ////////////////////////////////////
-    if($speechInput.val() != ''){
-        $('#testing').text($speechInput.val());
-    }
-    else{
-        if($speechInput.val() == ''){$('#testing').text("Estoy vacio");}
-        else{$('#testing').text("Aqui otra vez");}
-    }
-}
+// function printInput() {          //////////////////////////////////// PRINT FUNCTIONS ////////////////////////////////////
+//     if($speechInput.val() != ''){
+//         $('#testing').text($speechInput.val());
+//     }
+//     else{
+//         if($speechInput.val() == ''){$('#testing').text("Estoy vacio");}
+//         else{$('#testing').text("Aqui otra vez");}
+//     }
+// }
 
 function printLink(dato, dato2) {
         $('#testing').text('');
@@ -140,36 +135,26 @@ function send() {                //////////////////////////////////// SEND /////
         data: JSON.stringify({query: text, lang: "en", sessionId: "yaydevdiner"}),
         success: function(data) {
             prepareResponse(data);
-            // console.log("data from SEND");
-            // console.log(data);
         },
         error: function() {
             respond(messageInternalError);
         }
     });
     $('#statusMessages').text("Message Send!");
-    // firstTypedLetter = 'Y';
-    // if (auth === memberName )
-    // {
-    //$('#chatHistory').append( "<div ><span class='left'>"+auth+":"+msg+"</span></div><hr>" );
     datestr=getFormattedDate();
     chatHistoryDiv.append(
         "<div class='chat-message bubble-left' >"+
-        "<div class='avatar'>"+
-        "<img src='https://www.mytadvisor.com/SOA20/Profiles/defaultuser_SMALL.png' alt='' width='32' height='32' style='float: left;>"+
-        "</div>"+
-        "<div class='chat-message-content'>" +
-        "<h4 style='margin-top: 45px; margin-bottom: 0.5em; margin-left: 0.5em;'>"+text+"</h4>"+
-        "<h5 class='timestamp_right' style='font-size: 10px; margin-bottom: 0; margin-top: 0.5em'>"+datestr+"</h5>"+
-        //"<span class='chat-time right'>13:35</span>"+
-        //"<p class='right'>Me</p>"+
-        "</div> <!-- end chat-message-content -->"+
-        //"<hr>"+
+            "<div class='avatar'>"+
+                "<img src='https://www.mytadvisor.com/SOA20/Profiles/defaultuser_SMALL.png' alt='' width='32' height='32' style='float: left;>"+
+            "</div>"+
+            "<div class='chat-message-content'>" +
+                "<h4 style='margin-top: 45px; margin-bottom: 0.5em; margin-left: 0.5em;'>"+text+"</h4>"+
+                "<h5 class='timestamp_right' style='font-size: 10px; margin-bottom: 0; margin-top: 0.5em'>"+datestr+"</h5>"+
+            "</div> <!-- end chat-message-content -->"+
         "</div> <!-- end chat-message -->"
     );
     $("#chatHistory").animate({ scrollTop: $("#chatHistory")[0].scrollHeight}, 1000);
     $speechInput.val("");
-    ///////
 }
 
 function prepareResponse(val) {  //////////////////////////////////// RESPUESTA ////////////////////////////////////
@@ -198,33 +183,21 @@ function prepareResponse(val) {  //////////////////////////////////// RESPUESTA 
             // dataObj2 = JSON.stringify(dataObj,undefined,2).replace(/&nbsp/g,"");
             // spokenRespond(dataObj2);
             respond(dataObj);
-            // console.log(dataObj);
         }
         else if (spokenResponse[i].type==4 && spokenResponse[i].payload.items) { //type 4 is a custompayload
-            console.log(spokenResponse[i].payload.items);
-            // for(j=0;j<spokenResponse[i].payload.items.length;j++){
-            //     arrayList[j]=spokenResponse[i].payload.items[j]
-            // }
-            // printButton(arrayList);
             arrayList=spokenResponse[i].payload.items;
             printButton(arrayList);
         }
-
     }
-    // $('#testing').text(messagesPrint); //imprimir en testing label
     spokenRespond(messagesPrint);
-    // $('#testing').text(eval('\"'+ jsonEscape(spokenResponse[1].speech)+'\"'));
-
     ////// val.$1.$2 , se refiere a la respuesta JSON (valor) data.key1.key2
     if(val.result.metadata.intentName == "location"){
-
         location_c = val.result.parameters["geo-country"]; //por el dash "-" no se usa . punto
         news_country="https://news.google.com/news/search/section/q/"+location_c+"/"+location_c+"?hl=es-419&ned=es_co";
         link_country="<a href = "+news_country+" target =\"right_side\">News from "+location_c+"</a>";
         prueba="https://www.w3schools.com";
         frame_country="<iframe name=\"right_side\" src =\"\" width=\"300\" height=\"100\">mmmhhh</iframe>";
         printLink(link_country,frame_country);
-
     }
 }
 
@@ -238,19 +211,15 @@ function respond(val) {
         val = messageSorry;
     }
     datestr=getFormattedDate();
-    //$('#chatHistory').append( "<div ><span class='right'>"+auth+":"+msg+"</span></div><hr>" );
     chatHistoryDiv.append(
         "<div class='chat-message bubble-right'>"+
-        "<div class='avatar'>"+
-        "<img src='https://www.mytadvisor.com/SOA20/Content/Images/TAdvisor/isotipo.png' alt='' width='32x' height='32px' style='float: right;'>"+
-        "</div>"+
-        "<div class='chat-message-content' style= 'clear: right;'>" +
-        //"<span class='chat-time'>13:35</span>"+
-        // "<p font-size='50%' font-style= 'italic'>"+auth+"</p>"+
-        "<h4 style='margin-top: 45px; margin-bottom: 0.5em; margin-left: 0.5em;'>"+val+"</h4>"+
-        "<h5 class='timestamp_right' style='font-size: 10px; margin-bottom: 0; margin-top: 0.5em'>"+datestr+"</h5>"+
-        "</div> <!-- end chat-message-content -->"+
-        //"<hr>"+
+            "<div class='avatar'>"+
+                "<img src='https://www.mytadvisor.com/SOA20/Content/Images/TAdvisor/isotipo.png' alt='' width='32x' height='32px' style='float: right;'>"+
+            "</div>"+
+            "<div class='chat-message-content' style= 'clear: right;'>" +
+                "<h4 style='margin-top: 45px; margin-bottom: 0.5em; margin-left: 0.5em;'>"+val+"</h4>"+
+                "<h5 class='timestamp_right' style='font-size: 10px; margin-bottom: 0; margin-top: 0.5em'>"+datestr+"</h5>"+
+                "</div> <!-- end chat-message-content -->"+
         "</div> <!-- end chat-message -->"
     );
     if (val !== messageRecording) {
@@ -260,9 +229,7 @@ function respond(val) {
         msg.lang = "en-US";
         window.speechSynthesis.speak(msg);
     }
-    // $("#spokenResponse").addClass("is-active").find(".spoken-response__text").html(val);
-    // $("#spokenResponseTitle").addClass("is-actived").find(".responseLabel").html("API Response");
-
+    $speechInput.focus();
 }
 
 function spokenRespond (val){
@@ -328,12 +295,9 @@ function send_event() {                //////////////////////////////////// SEND
 function printButton(arrayList){
     var printButton_i="<br />";
     var chatHistoryDiv = $("#chatHistory");
-
-    console.log(arrayList);
-
     for(i=0;i<arrayList.length;i++){
         buttonId[i]=createIdFromText(arrayList[i]);
-        printButton_i+="<div class='quickReplyButton' style='display:inline'>"+"<button id='"+buttonId[i]+"' name='listButton"+i+"' onclick=\"quickReplyF('"+arrayList[i]+"',"+'arrayList'+","+'buttonId'+")\" style='margin-top: 5px; display: inline-block;'>"+arrayList[i]+"</button>"+"</div>";
+        printButton_i+="<div class='quickReplyButton' style='display:inline'>"+"<button id='"+buttonId[i]+"' name='listButton"+i+"' onclick=\"quickReplyF('"+arrayList[i]+"',"+'buttonId'+")\" style='margin-top: 5px; display: inline-block;'>"+arrayList[i]+"</button>"+"</div>";
     }
     // console.log(printButton_i);
     datestr=getFormattedDate();
@@ -349,11 +313,10 @@ function printButton(arrayList){
     // return printButton_i;
 }
 
-function quickReplyF(stringItem,arrayList){
+function quickReplyF(stringItem,buttonId){
     $speechInput.val(stringItem);
-    console.log(arrayList);
+    // disableButtons(buttonId);
     send();
-    disableButtons(buttonId);
 }
 
 function disableButtons(buttonId){
@@ -361,6 +324,7 @@ function disableButtons(buttonId){
         document.getElementById(buttonId[i]).disabled = true;
     }
 }
+
 function createIdFromText(idText){
     return idText.toLowerCase().replace(/ /g,"");
 }
