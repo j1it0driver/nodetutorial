e8c2383ec5055362757e7bde414aad529638f91d
TR JDO Ubuntu <j1it0driver@gmail.com>
2017-10-18 14:20:15 +0200

    testing assettypes handler

--- routes/webhook.js
+++ routes/webhook.js
@@ -13,12 +13,12 @@ var api = require('./api');
 // var app = apiai(accessToken);
 var app = apiai("aba2ecdbb9e744ba8b37ec6cf6a175d9");
 var router = express.Router();
-var cookies = global.cookies_s;
+// var cookies = global.cookies_s;
 /* GET ex:"users" listing. */
 router.post('/', function(req, res) { //api.ai for nodejs
 
     console.log('Request to webhook: ', req.body);
-    console.log('cookies from client', cookies);
+    console.log('cookies from client', global.cookies_s);
     util.fulfillment(req, res);
     console.log('Response from webhook',res);
 //     var cookies_s = cookie.parse(req.headers.cookie || '');
