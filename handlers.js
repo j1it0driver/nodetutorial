c9c0b3b04767e801cfcf3c0d08ecbb1dab2ee908
TR JDO Ubuntu <j1it0driver@gmail.com>
2017-10-19 10:40:21 +0200

    testing assettypes handler3

--- handlers.js
+++ handlers.js
@@ -92,8 +92,9 @@ var GetMyTAdvisorScreenerProductTypesHandler = function(){
     console.log("Enter action");
     var userCode="userCode", domain, language, token;
     console.log("Cookies Servidor", cookies_s);
-    console.log("Cookies Servidor 1", cookies_s["userCode"];
+    console.log("Cookies Servidor 1", cookies_s["userCode"]);
     console.log("Cookies Servidor 2", cookies_s.userCode);
+
     if (cookiesm.checkCookieServer("userCode") && cookiesm.checkCookieServer("tokenString")){
         userCode=cookiesm.readCookieServer("userCode");
         domain="TADVISOR";
