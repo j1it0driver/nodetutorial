b66c0018e4efc79b1eb502d51711242f758db002
TR JDO Ubuntu <j1it0driver@gmail.com>
2017-10-20 12:51:03 +0200

    webhook from client2

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -994,7 +994,7 @@ function printAssets(data){
 // ]
     $("</br><form class='radios' id='chatBubbleDivDiv"+printIndex+"'></form>").appendTo('#chatBubbleDiv'+printIndex);
     for(i=0;i<data.length;i++){
-        $("<div id='radio"+i+1+""+printIndex+"'class='radio'><label><input type='radio' name='optradio' value=''><span>&nbsp;&nbsp;&nbsp;<strong>"+data[i].Name+"</strong></span></br><span>&nbsp;&nbsp;&nbsp;&nbsp;<i>Last Price:&nbsp;</i>"+data[i].LastPrice+"</span></br><span>&nbsp;&nbsp;&nbsp;&nbsp;<i>ISIN:&nbsp;</i>"+data[i].Isin+"</span></label></div>").appendTo('#chatBubbleDivDiv'+printIndex);
+        $("<div id='radio"+i+1+""+printIndex+"'class='radio'><label><input type='radio' name='optradio' value=''><span>&nbsp;&nbsp;&nbsp;<strong>"+data[i].Name+"</strong></span></br><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>Last Price:&nbsp;&nbsp;&nbsp;</i>"+data[i].LastPrice+"</span>&nbsp;&nbsp;<span>"+data[i].Currency+"</span></br><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>ISIN:&nbsp;&nbsp;&nbsp;</i>"+data[i].Isin+"</span></label></div>").appendTo('#chatBubbleDivDiv'+printIndex);
     }
 
 }
