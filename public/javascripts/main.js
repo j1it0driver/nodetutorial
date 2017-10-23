3cff2d343ea415c40faff7e2150cf468ff2ee41e
TR JDO Ubuntu <j1it0driver@gmail.com>
2017-10-23 10:29:00 +0200

    webhook from client

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1001,6 +1001,7 @@ function printAssets(data){
     for(i=0;i<data.length;i++){
         radiosId.push("radio"+i+""+printIndex);
         $("<div id='radio"+i+""+printIndex+"'class='radio'><label><input type='radio' name='optradio' value='"+data[i].Isin+"'><span>&nbsp;&nbsp;&nbsp;<strong>"+data[i].Name+"</strong></span></br><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>Last Price:&nbsp;&nbsp;&nbsp;</i>"+data[i].LastPrice+"</span>&nbsp;&nbsp;<span>"+data[i].Currency+"</span></br><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>ISIN:&nbsp;&nbsp;&nbsp;</i>"+data[i].Isin+"</span></label></div>").appendTo('#chatBubbleDivDiv'+printIndex);
+        console.log(radiosId[i]);
     }
     addMessage("If the asset is not listed, please be more specific");
 
