53e4138907b70654d4ad06c1407cf9852afdfb83
TR JDO Ubuntu <j1it0driver@gmail.com>
2017-11-13 11:51:06 +0100

    mailing from server

--- routes/sayHello.js
+++ routes/sayHello.js
@@ -5,46 +5,57 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/', function(req,res){
     console.log("print body", req.body);
-    var transporter = nodemailer.createTransport({
-        host: "smtp.live.com", // hostname
-        secureConnection: false, // TLS requires secureConnection to be false
-        port: 587, // port for secure SMTP
-        tls: {
-           ciphers:'SSLv3'
-        },
-        auth: {
-            user: 'juanxdario@hotmail.com',
-            pass: 'KellyMarie2017$ho'
-        }
-    });
 
-    var mailOptions = {
-        from: '"'+req.body.Name+'" <'+req.body.email+'>', // sender address
-        to: 'jdortiz@techrules.com', // list of receivers
-        subject: req.body.subject+" from user: " +req.body.Name, // Subject line
-        text: req.body.body //, // plaintext body
-        // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
-    };
+    // Generate test SMTP service account from ethereal.email
+// Only needed if you don't have a real mail account for testing
+    nodemailer.createTestAccount((err, account) => {
+        var transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.com", // hostname
+            secureConnection: false, // TLS requires secureConnection to be false
+            port: 587, // port for secure SMTP
+            tls: {
+               ciphers:'SSLv3'
+            },
+            auth: {
+                user: 'account.user',
+                pass: 'account.pass'
+            }
+        });
 
-    console.log("mailOptions", mailOptions);
-
-    // verify connection configuration
-    transporter.verify(function(error, success) {
-       if (error) {
-            console.log("verify error",error);
-       } else {
-            console.log('Server is ready to take our messages');
-       }
-    });
-
-    transporter.sendMail(mailOptions, function(error, info){
-        if(error){
-            console.log("error",error);
-            res.json({'yo': 'error'});
-        }else{
-            console.log('Message sent: ' + info.response);
-            res.json({'yo': info.response});
+        var mailOptions = {
+            from: '"'+req.body.Name+'" <'+req.body.email+'>', // sender address
+            to: 'jdortiz@techrules.com', // list of receivers
+            subject: req.body.subject+" from user: " +req.body.Name, // Subject line
+            text: req.body.body //, // plaintext body
+            // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
         };
+
+        console.log("mailOptions", mailOptions);
+
+        // verify connection configuration
+        transporter.verify(function(error, success) {
+           if (error) {
+                console.log("verify error",error);
+           } else {
+                console.log('Server is ready to take our messages');
+           }
+        });
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+                console.log("error",error);
+                res.json({'yo': 'error'});
+            }else{
+                console.log('Message sent: ' + info.response);
+                res.json({'yo': info.response});
+                console.log('Message sent: %s', info.messageId);
+                // Preview only available when sending through an Ethereal account
+                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+                // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+                // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+            };
+        });
     });
 });
 
