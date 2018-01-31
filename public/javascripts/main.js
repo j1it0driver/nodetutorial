23bd0bba31a697f1e53c818e220f00cbf357e0c7
Macbook JD <j1it0driver@gmail.com>
2018-01-31 18:22:53 +0100

    postMessage tests

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -81,7 +81,7 @@ $(document).ready(function() {
     console.log("Browser/OS:", bowser.name, bowser.osname);
     console.log("iOS", iOS);
     console.log("iOS device", _iOSDevice);
-    console.log("prueba inside main",myServerData);
+    console.log("prueba inside main",myServerData); 
     // console.log(uSession);
     visits();
     username();
