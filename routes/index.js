6a5419bc4c4abf201fbca99ef2866a1e11e10afc
Macbook JD <j1it0driver@gmail.com>
2018-02-06 13:28:31 +0100

    userEvaluation function

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var router = express.Router();
 //   res.render('index', { title: 'Express' });
 // });
 router.get('/', function(req,res){
-  console.log("es la llamada a route index.js",req);
+  console.log("es la llamada a route index.js");
   // console.log("Cookies :  ", req.cookies);
   // var cookies_s = cookie.parse(req.headers.cookie || '');
   // console.log(cookies_s.visits);
