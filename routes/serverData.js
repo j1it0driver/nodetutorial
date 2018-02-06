d0cd801fa0df0196f32ca8ceb8927fddefc93ba1
Macbook JD <j1it0driver@gmail.com>
2018-02-06 13:08:17 +0100

    userEvaluation function

--- routes/serverData.js
+++ routes/serverData.js
@@ -4,18 +4,8 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/', function(req,res){
     console.log("print body from client myServerData on server", req.body);
-    
-    res.json({'updated': 'yes'});
 
-        
-        // var mailOptions = {
-            
-        //     from: req.body.email,
-        //     to: 'jdortiz@techrules.com', // list of receivers
-        //     subject: req.body.subject+" User: " +req.body.name+ ". Reference #: "+reference, // Subject line
-        //     text: req.body.body //, // plaintext body
-            
-        // };
+    res.json({'updated': 'yes'});
 
 });
 
