0965d098a4d6996f0d56b52e6e845e875cd0d7a6
Macbook JD <j1it0driver@gmail.com>
2018-02-20 22:35:21 +0100

    userEvaluation

--- wk.js
+++ wk.js
@@ -349,7 +349,7 @@ var  fulfillment = function(req, res){ //Raphael Meudec API.AI Facebook Messenge
                 var call = https.request(options, (response) => {
                     response.on('data', (chunk) => {
                         //global.userEvalResult= JSON.parse(chunk.toString()).RSLT.DATA;
-                        lobal.userEvalResult= chunk.RSLT.DATA;
+                        global.userEvalResult= chunk.RSLT.DATA;
                         console.log("SSwk userEvalResult",userEvalResult);
                     });
                     response.on('end', ()=> {
